bfb551970ff0bb9bd5982753b63b938729c3d45b
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2008-12-28 15:30:27 +0000

    populate_textview() can handle all kinds of XML::LibXML nodes

--- lib/Xacobeo/XS.pm
+++ lib/Xacobeo/XS.pm
@@ -30,6 +30,13 @@ use Glib qw(TRUE FALSE);
 use Gtk2;
 use XML::LibXML;
 
+use Xacobeo::Utils qw(
+	isa_dom_nodelist
+	isa_dom_boolean
+	isa_dom_number
+	isa_dom_literal
+);
+
 sub dl_load_flags {0x01};
 
 __PACKAGE__->bootstrap;
@@ -73,15 +80,67 @@ sub populate_textview {
 	
 	# Clear the buffer
 	$buffer->delete($buffer->get_start_iter, $buffer->get_end_iter);
-	
-	# Populate the buffer and swapt it back into the text view
-	xacobeo_populate_gtk_text_buffer($buffer, $node, $namespaces);
+
+
+	# A NodeList
+	if (isa_dom_nodelist($node)) {
+		my @children = $node->get_nodelist;
+		my $count = scalar @children;
+
+		# Formatting using to indicate which result is being analyzed
+		my $i = 0;
+		my $format = sprintf " %%%dd. ", length($count);
+
+		foreach my $child (@children) {
+			# Add the result count
+			my $result = sprintf $format, ++$i;
+			buffer_add($buffer, result_count => $result);
+			
+			# Print the current child (XS call)
+			xacobeo_populate_gtk_text_buffer($buffer, $child, $namespaces);
+			
+			buffer_add($buffer, syntax => "\n") if --$count;
+		}
+	}
+
+	# A Boolean value ex: true() or false()
+	elsif (isa_dom_boolean($node)) {
+		buffer_add($buffer, boolean => $node->to_literal);
+	}
+
+	# A Number ex: 2 + 5
+	elsif (isa_dom_number($node)) {
+		buffer_add($buffer, number => $node->to_literal);
+	}
+
+	# A Literal (a single text string) ex: "hello"
+	elsif (isa_dom_literal($node)) {
+		buffer_add($buffer, literal => $node->to_literal);
+	}
+
+	else {
+		# Any kind of XML node (XS call)
+		xacobeo_populate_gtk_text_buffer($buffer, $node, $namespaces);
+	}
+
+
+	# Add the buffer back into into the text view
 	$textview->set_buffer($buffer);
 
 	# Scroll to tbe beginning
 	$textview->scroll_to_iter($buffer->get_start_iter, 0.0, FALSE, 0.0, 0.0);
 }
 
+
+
+#
+# Adds the given text at the end of the buffer. The text is added with a tag
+# which can be used for performing syntax highlighting.
+#
+sub buffer_add {
+	my ($buffer, $tag, $string) = @_;
+	$buffer->insert_with_tags_by_name($buffer->get_end_iter, $string, $tag);
+}
 
 
 =head2 populate_treeview
@@ -118,6 +177,7 @@ sub populate_treeview {
 	my $store = $treeview->get_model;
 	
 	$treeview->set_model(undef);
+	# (XS call)
 	xacobeo_populate_gtk_tree_store($store, $node, $namespaces);
 	$treeview->set_model($store);
 }
