2a6602cdb76d74d579076cc90a0152eda5ebb202
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2008-12-28 15:10:15 +0000

    Clearing the text buffer

--- lib/Xacobeo/XS.pm
+++ lib/Xacobeo/XS.pm
@@ -66,16 +66,22 @@ the URIs and the values the prefixes of the namespaces.
 
 sub populate_textview {
 	my ($textview, $node, $namespaces) = @_;
-	my $buffer = $textview->get_buffer;
 
 	# It's faster to disconnect the buffer from the view and to reconnect it back
+	my $buffer = $textview->get_buffer;
 	$textview->set_buffer(Gtk2::TextBuffer->new());# Perl-Gk2 Bug can't set undef as a buffer
+	
+	# Clear the buffer
+	$buffer->delete($buffer->get_start_iter, $buffer->get_end_iter);
+	
+	# Populate the buffer and swapt it back into the text view
 	xacobeo_populate_gtk_text_buffer($buffer, $node, $namespaces);
 	$textview->set_buffer($buffer);
 
 	# Scroll to tbe beginning
 	$textview->scroll_to_iter($buffer->get_start_iter, 0.0, FALSE, 0.0, 0.0);
 }
+
 
 
 =head2 populate_treeview
