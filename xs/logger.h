8b8afa99b1234dfb477756c6d20b80e1ae2a6767
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2009-03-03 12:29:26 +0000

    The high level logger functions are always available

--- xs/logger.h
+++ xs/logger.h
@@ -1,22 +1,29 @@
 #ifndef __XACOBEO_LOGGER_H
 #define __XACOBEO_LOGGER_H
 
+#define LOG(level, ...) my_logger_log(__FILE__, __LINE__, __FUNCTION__, level, __VA_ARGS__)
+
+// Pseudo Log4c
 #ifdef HAS_DEBUG
-#  define LOG(level, ...) my_logger_log(__FILE__, __LINE__, __FUNCTION__, level, __VA_ARGS__)
+#  define TRACE(...) LOG("TRACE", __VA_ARGS__)
+#  define DEBUG(...) LOG("DEBUG", __VA_ARGS__)
+#  define INFO(...)  LOG("INFO",  __VA_ARGS__)
+#  define NOTE(...)  LOG("NOTE",  __VA_ARGS__)
 #else
 // Trick the compiler by pretending that we are using the statement. Otherwise
 // it can issue warnings regarding unused variables.
-#  define LOG(level,...) if (0) my_logger_log(__FILE__, __LINE__, __FUNCTION__, level, __VA_ARGS__)
+#  define LOG_NOOP(...) if (0) LOG("noop", __VA_ARGS__)
+#  define TRACE(...)    LOG_NOOP(__VA_ARGS__)
+#  define DEBUG(...)    LOG_NOOP(__VA_ARGS__)
+#  define INFO(...)     LOG_NOOP(__VA_ARGS__)
+#  define NOTE(...)     LOG_NOOP(__VA_ARGS__)
 #endif
 
 
-// Pseudo Log4c
-#define TRACE(...) LOG("TRACE", __VA_ARGS__)
-#define DEBUG(...) LOG("DEBUG", __VA_ARGS__)
-#define INFO(...)  LOG("INFO",  __VA_ARGS__)
-#define NOTE(...)  LOG("NOTE",  __VA_ARGS__)
+// This logging levels are always available
 #define WARN(...)  LOG("WARN",  __VA_ARGS__)
 #define ERROR(...) LOG("ERROR", __VA_ARGS__)
+
 
 //
 // Prototypes
